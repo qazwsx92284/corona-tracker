c796d594ff3481d681a4abbf74aab95979a9dc12
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-09-02 19:14:44 -0700

    initial commit - creation of base spring boot project

---
+++ src/test/java/com/min0/corona/CoronaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min0.corona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
